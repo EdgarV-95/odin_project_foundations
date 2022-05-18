64e2ad4ab1b9fd148a08afc8d24d14833832ff1f
EdgarV-95 <edgar.veress@yahoo.com>
2022-05-18 17:42:04 +0100

    various tests

---
+++ tests/script.js
@@ -0,0 +1,55 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+container.appendChild(content);
+
+const pharagraph = document.createElement('p');
+pharagraph.textContent = "Hey I'm red!";
+container.appendChild(pharagraph);
+
+const h3_element = document.createElement('h3');
+h3_element.textContent = "I'm blue h3!";
+container.appendChild(h3_element);
+
+const another_content = document.createElement('div');
+another_content.style.backgroundColor = 'pink';
+another_content.style.border = 'black';
+
+const h_element = document.createElement('h1');
+h_element.textContent = "I'm in a div";
+
+const p_2 = document.createElement('p');
+p_2.textContent = "ME TOO!";
+
+another_content.appendChild(h_element)
+another_content.appendChild(p_2)
+
+container.appendChild(another_content);
+
+//////////////////////////////////////////////////
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello World');
+
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+  });
+
+btn.addEventListener('click', function (e) {
+e.target.style.background = 'blue';
+});
+
+
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         alert(button.id);
+//     });
+// });
+
+const that_one = document.querySelector('.test');
+that_one.addEventListener('click', () => {
+    console.log('Ye buddy!');
+});
