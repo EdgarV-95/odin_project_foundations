7999eb34168666b05dc513aba843aa347d536920
EdgarV-95 <edgar.veress@yahoo.com>
2022-05-29 15:55:26 +0100

    finished the logic part. No UI yet

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -14,36 +14,77 @@ let rock = document.getElementById('rock-btn');
 let paper = document.getElementById('paper-btn');
 let scissors = document.getElementById('scissors-btn');
 
+let result = document.getElementById('result');
+let runningScore = document.getElementById('running-score');
+let finalResult = document.getElementById('finalResult');
+
 rock.addEventListener('click', playRound);
 paper.addEventListener('click', playRound);
 scissors.addEventListener('click', playRound);
 
 // Plays a whole round of the game and console logs the winner. It also updates the global variables in case multiple rounds will be played.
 function playRound(e, computerSelection) {
-    playerSelection = prompt('Choose one from Rock, Paper or Scissors').toLowerCase();
+    playerSelection = e.target.value;
     computerSelection = computerPlay().toLowerCase();
-    if (playerSelection == "rock" && computerSelection == "rock") {console.log("Draw! You both selected Rock"); draw += 1}
-    else if (playerSelection == "rock" && computerSelection == "scissors") {console.log( "You Win! Rock beats Scissors"); win += 1}
-    else if (playerSelection == "rock" && computerSelection == "paper") {console.log("You Lose! Paper beats Rock"); lose += 1}
-    else if (playerSelection == "paper" && computerSelection == "paper") {console.log("Draw! You both selected Paper"); draw += 1}
-    else if (playerSelection == "paper" && computerSelection == "rock") {console.log("You Win! Paper beats Rock"); win += 1}
-    else if (playerSelection == "paper" && computerSelection == "scissors") {console.log("You Lose! Scissors beats Paper"); lose += 1}
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {console.log("Draw! You both selected Scissors"); draw += 1}
-    else if (playerSelection == "scissors" && computerSelection == "paper") {console.log("You Win! Scissors beat Paper"); win += 1}
-    else if (playerSelection == 'scissors' && computerSelection == "rock") {console.log("You Lose! Rock beats Scissors"); lose += 1}
-    else console.log('There was an error');
+    runningScore.innerText = `Draw: ${draw} || Win: ${win} || Lose: ${lose}`;
+
+    if (win == 5) {
+        return finalResult.innerText = `Congratulations you Won!`;
+    };
+    if (lose == 5) {
+        return finalResult.innerText = `Sorry, you Lost!`;
+    };
+    if (draw == 5) {
+        return finalResult.innerText = `It is a Draw!`;
+    };
+
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        result.innerText = "Draw! You both selected Rock";
+        draw += 1
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        result.innerText = "You Win! Rock beats Scissors";
+        win += 1
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        result.innerText = "You Lose! Paper beats Rock";
+        lose += 1
+    }
+    else if (playerSelection == "paper" && computerSelection == "paper") {
+        result.innerText = "Draw! You both selected Paper";
+        draw += 1
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        result.innerText = "You Win! Paper beats Rock";
+        win += 1
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        result.innerText = "You Lose! Scissors beats Paper";
+        lose += 1
+    }
+    else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        result.innerText = "Draw! You both selected Scissors";
+        draw += 1
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        result.innerText = "You Win! Scissors beat Paper";
+        win += 1
+    }
+    else if (playerSelection == 'scissors' && computerSelection == "rock") {
+        result.innerText = "You Lose! Rock beats Scissors";
+        lose += 1
+    }
+    else result.innerText = 'There was an error';
+
 }
 
-// Plays multiple rounds of the game depending on the for loop. Prints out the winner and final score of the game.
-function game() {
-    // Plays 5 rounds
-    // for (let i = 0; i < 5; i++) {
-    //     playRound();
-    // }
-    if (win > lose && draw) {console.log("Congratulations! You won")}
-    else if (lose > win && draw) {console.log("Sorry, you lost")}
-    else {console.log("It is a draw!")}
-    console.log(`Total wins: ${win} || Total losses: ${lose} || Total draws: ${draw}`);
+function whoWins() {
+    
 }
 
-game();
+let refreshButton = document.getElementById('again')
+refreshButton.addEventListener('click', reloadPage);
+
+function reloadPage() {
+    location.reload();
+}
