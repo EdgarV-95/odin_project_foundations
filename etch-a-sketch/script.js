26953d3753f87f1dcfd09325516cdf73549d7107
EdgarV-95 <edgar.veress@yahoo.com>
2022-05-31 01:50:40 +0100

    Added createGrid function

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,37 +1,21 @@
-let container = document.getElementById('container');
-
 let gridSize = 16;
 
 function createGrid() {
+    let container = document.getElementById('container');
+
     for (let i = 0; i < gridSize; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row-test')
-        let textA = document.createTextNode('ROW ');
-        row.appendChild(textA);
-        for(let j = 1; j < gridSize; j++) {
-            let column = document.createElement('span');
-            column.classList.add('column-test')
-            let textB = document.createTextNode('COLUMN ');
-            column.appendChild(textB);
-            row.appendChild(column);
-        };
+        let row = document.createElement('tr');
+        row.id = 'row'+ i;
+
         container.appendChild(row);
-    };
+        let column = document.getElementById('row'+ i);
+
+        for (let j = 0; j < gridSize; j++) {
+            let createColumn = document.createElement('td');
+            createColumn.style.border = '1pt solid black;'
+            column.appendChild(createColumn);
+        }
+    }
 };
-
-let rowColors = document.getElementsByClassName('row-test');
-let columnColors = document.getElementsByClassName('column-test');
-
-// rowColors.forEach(element => {
-//     element.addEventListener('mouseover', changeColor)
-// });
-
-// columnColors.forEach(element => {
-//     element.addEventListener('mouseover', changeColor)
-// });
-
-// function changeColor(e) {
-//     e.style.color = 'blue';
-// }
 
 createGrid();
