5363b85378ff89a85a7b27b6f9c7ba9b96789fc5
EdgarV-95 <edgar.veress@yahoo.com>
2022-06-01 19:19:44 +0100

    added resetColor function

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,16 +1,14 @@
-let gridSize = 32;
-
-function createGrid() {
+function createGrid(rowSize, columnSize) {
     let container = document.getElementById('container');
 
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < rowSize; i++) {
         let row = document.createElement('tr');
         row.id = 'row' + i;
 
         container.appendChild(row);
         let createColumn = document.getElementById('row'+ i);
 
-        for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < columnSize; j++) {
             let column = document.createElement('td');
             column.id = 'column' + j;
             column.classList = 'grid-item';
@@ -19,15 +17,24 @@ function createGrid() {
     };
 };
 
-createGrid();
+createGrid(32, 32);
 
-document.addEventListener('mouseover', changeColor)
+let myNodeList = document.querySelectorAll('td');
 
 function changeColor(e) {
-    let myNodeList = document.querySelectorAll('td');
     Array.from(myNodeList).forEach(el => {
         if (e.target == el) {
             e.target.style.backgroundColor = 'red';
         }
     })
 }
+
+document.addEventListener('mouseover', changeColor);
+
+function resetColor() {
+    Array.from(myNodeList).forEach(el => {
+            el.style.backgroundColor = 'white';
+        })
+}
+
+document.getElementById('reset').addEventListener('click', resetColor);
