cdab207af13087cf97585bf84bf8f33ca0a83a5c
EdgarV-95 <edgar.veress@yahoo.com>
2022-06-02 00:36:45 +0100

    updated create-grid function, small style and html changes

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,3 +1,4 @@
+// Creates custom grid where the row and column sizes are definable
 function createGrid(rowSize, columnSize) {
     let container = document.getElementById('container');
 
@@ -17,24 +18,33 @@ function createGrid(rowSize, columnSize) {
     };
 };
 
-createGrid(32, 32);
+createGrid(12,12)
 
+// Selects all td elements and saves it in variable
 let myNodeList = document.querySelectorAll('td');
 
+// Change color from default
 function changeColor(e) {
     Array.from(myNodeList).forEach(el => {
         if (e.target == el) {
-            e.target.style.backgroundColor = 'red';
+            e.target.style.backgroundColor = currentColor;
         }
     })
 }
-
 document.addEventListener('mouseover', changeColor);
 
+// Change to various colors
+let currentColor = document.getElementById("color-selector").value;
+document.getElementById("color-selector").oninput = function() {
+    currentColor = this.value;
+}
+
+// Reset colors
 function resetColor() {
     Array.from(myNodeList).forEach(el => {
             el.style.backgroundColor = 'white';
         })
+    document.getElementById("color-selector").value = '#000000'
+    currentColor = '#000000';
 }
-
-document.getElementById('reset').addEventListener('click', resetColor);
+document.getElementById('reset-btn').addEventListener('click', resetColor);
