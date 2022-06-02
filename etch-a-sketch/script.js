934f9ba677159abe9f71175f3b0983b09a505e99
Edgar Veress <veress.edgar@yahoo.com>
2022-06-02 22:14:25 +0100

    small html and css changes

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,6 +1,13 @@
+let container = document.getElementById('container');
+let currentColor = document.getElementById("color-selector").value;
+let gridSize = document.getElementById('grid-size');
+let colorSelector = document.getElementById("color-selector");
+let boardReset = document.getElementById('reset-board');
+let colorReset = document.getElementById('reset-color');
+
 // Creates custom grid where the row and column sizes are definable
 function createGrid(gridSize) {
-    let container = document.getElementById('container');
+    // Empties the container
     container.innerHTML = ' ';
 
     for (let i = 0; i < gridSize; i++) {
@@ -20,33 +27,33 @@ function createGrid(gridSize) {
     };
 };
 
-function changeColor() {
-    this.style.backgroundColor = currentColor;
-};
-
-createGrid(12);
-
 // Change grid size
-document.getElementById('grid-size').addEventListener('click', changeGridSize);
+gridSize.addEventListener('click', changeGridSize);
 function changeGridSize() {
     let input = prompt('What grid size would you like?');
     createGrid(input);
 }
 
 // Change to various colors
-let currentColor = document.getElementById("color-selector").value;
-document.getElementById("color-selector").oninput = selectColor;
+colorSelector.oninput = selectColor;
 function selectColor() {
     currentColor = this.value;
 }
 
-// Reset colors
-function resetColor() {
+// Reset board
+boardReset.addEventListener('click', resetBoard);
+function resetBoard() {
     let myNodeList = document.querySelectorAll('td');
     Array.from(myNodeList).forEach(el => {
             el.style.backgroundColor = 'white';
         })
+}
+
+// Reset color
+colorReset.addEventListener('click', resetColor);
+function resetColor() {
     document.getElementById("color-selector").value = '#000000'
     currentColor = '#000000';
 }
-document.getElementById('reset-btn').addEventListener('click', resetColor);
+
+createGrid(12);
