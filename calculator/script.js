b323b8ca1cf2739d19a15f7384404400dfe4418d
Edgar Veress <veress.edgar@yahoo.com>
2022-06-19 21:41:05 +0100

    calculator project

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+// add
+const add = (a, b) => a + b;
+
+// subtract
+const subtract = (a, b) => a - b;
+
+// multiply
+const multiply = (a ,b) => a * b;
+
+// divide
+const divide = (a, b) => a / b;
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+const operate = (operator, a, b) => {
+    return operator(a,b);
+};
+
+// Create the functions that populate the display when you click the number buttons…
+// you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+const populate = () => {
+    
+}
