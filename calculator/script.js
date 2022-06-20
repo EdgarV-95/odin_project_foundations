342dc83ffba64e9ae510399bd8f140189760651d
EdgarV-95 <edgar.veress@yahoo.com>
2022-06-20 12:47:14 +0100

    starting step 5

--- calculator/script.js
+++ calculator/script.js
@@ -1,24 +1,8 @@
-// add
-const add = (a, b) => a + b;
+// Display fields
+const displayTotal = document.getElementById('display-total')
+const displayCurrent = document.getElementById('display-current')
 
-// subtract
-const subtract = (a, b) => a - b;
-
-// multiply
-const multiply = (a, b) => a * b;
-
-// divide
-const divide = (a, b) => a / b;
-
-// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-const operate = (operator, a, b) => {
-    return operator(a,b);
-};
-
-// Create the functions that populate the display when you click the number buttons…
-// you should be storing the ‘display value’ in a variable somewhere for use in the next step.
-
-const display = document.getElementById('.display')
+// Digits
 const zero = document.getElementById('zero');
 const one = document.getElementById('one');
 const two = document.getElementById('two');
@@ -30,9 +14,76 @@ const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 
-console.log(zero.value);
-// zero.addEventListener('click', populate)
+// Operations
+const addition = document.getElementById('add');
+const subtraction = document.getElementById('substract');
+const multiplication = document.getElementById('mulitply');
+const division = document.getElementById('divide');
+const equals = document.getElementById('equal');
+const clear = document.getElementById('clear');
 
-// const populate = () => {
-//     console.log(zero.innerText);
+// Add
+const add = (a, b) => a + b;
+
+// Subtract
+const subtract = (a, b) => a - b;
+
+// Multiply
+const multiply = (a, b) => a * b;
+
+// Divide
+const divide = (a, b) => a / b;
+
+// Create a new function that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+const operate = (operator, a, b) => {
+    return operator(a,b);
+};
+
+// Create the functions that populate the display when you click the number buttons
+// Store the ‘display value’ in a variable somewhere for use in the next step.
+let displayValue;
+const populate = (e) => {
+    displayCurrent.textContent = e.target.outerText;
+    displayValue = +displayCurrent.textContent;
+}
+
+zero.addEventListener('click', populate);
+one.addEventListener('click', populate);
+two.addEventListener('click', populate);
+three.addEventListener('click', populate);
+four.addEventListener('click', populate);
+five.addEventListener('click', populate);
+six.addEventListener('click', populate);
+seven.addEventListener('click', populate);
+eight.addEventListener('click', populate);
+nine.addEventListener('click', populate);
+
+// Store the first number that is input into the calculator when a user presses an operator,
+// and also save which operation has been chosen 
+// and then operate() on them when the user presses the “=” key.
+function saveLast() {
+    let firstNumber = displayValue;
+    console.log(firstNumber);
+
+}
+
+// function calculator() {
+//     let firstNumber = displayValue;
+//     console.log(firstNumber);
+//     let operator;
 // }
+
+addition.addEventListener('click', saveLast)
+subtraction.addEventListener('click', saveLast)
+multiplication.addEventListener('click', saveLast)
+division.addEventListener('click', saveLast)
+
+
+
+
+// You should already have the code that can populate the display,
+// so once operate() has been called, update the display with the ‘solution’ to the operation.
+
+// This is the hardest part of the project. 
+// You need to figure out how to store all the values and call the operate function with them.
+// Don’t feel bad if it takes you a while to figure out the logic.
