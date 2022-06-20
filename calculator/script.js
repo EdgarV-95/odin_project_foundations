5c232fdaa8e2a884b7610bf5a7c29408332f5869
Edgar Veress <veress.edgar@yahoo.com>
2022-06-20 09:58:26 +0100

    created variables for every digit

--- calculator/script.js
+++ calculator/script.js
@@ -5,7 +5,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
 // multiply
-const multiply = (a ,b) => a * b;
+const multiply = (a, b) => a * b;
 
 // divide
 const divide = (a, b) => a / b;
@@ -17,6 +17,20 @@ const operate = (operator, a, b) => {
 
 // Create the functions that populate the display when you click the number buttons…
 // you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+const zero = document.getElementById('#zero');
+const one = document.getElementById('#one');
+const two = document.getElementById('#two');
+const three = document.getElementById('#three');
+const four = document.getElementById('#four');
+const five = document.getElementById('#five');
+const six = document.getElementById('#six');
+const seven = document.getElementById('#seven');
+const eight = document.getElementById('#eight');
+const nine = document.getElementById('#nine');
+
+const display = document.getElementById('.display')
+display.addEventListener('click', populate)
 
 const populate = () => {
     
