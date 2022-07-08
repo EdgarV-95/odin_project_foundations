51268bb4dce828acfda29fda04be030b7ac2ba0f
EdgarV-95 <edgar.veress@yahoo.com>
2022-07-08 01:15:40 +0100

    js cleanup and created style.css file

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,5 @@
 // This functions is for the computer to return one of the 3 options randomly from the array "arr".
-function computerPlay() {
+const computerPlay = () => {
     let arr = ['Rock', 'Paper', 'Scissors'];
     return arr[Math.floor(Math.random() * 3)];
 }
@@ -18,14 +18,10 @@ let result = document.getElementById('result');
 let runningScore = document.getElementById('running-score');
 let finalResult = document.getElementById('finalResult');
 
-rock.addEventListener('click', playRound);
-paper.addEventListener('click', playRound);
-scissors.addEventListener('click', playRound);
-
 // Plays a whole round of the game and console logs the winner. It also updates the global variables in case multiple rounds will be played.
-function playRound(e, computerSelection) {
+const playRound = (e) => {
     playerSelection = e.target.value;
-    computerSelection = computerPlay().toLowerCase();
+    let computerSelection = computerPlay().toLowerCase();
     runningScore.innerText = `Draw: ${draw} || Win: ${win} || Lose: ${lose}`;
 
     if (win == 5) {
@@ -41,41 +37,45 @@ function playRound(e, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "rock") {
         result.innerText = "Draw! You both selected Rock";
         draw += 1;
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
+    };
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         result.innerText = "You Win! Rock beats Scissors";
         win += 1;
-    }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
+    };
+    if (playerSelection == "rock" && computerSelection == "paper") {
         result.innerText = "You Lose! Paper beats Rock";
         lose += 1;
-    }
-    else if (playerSelection == "paper" && computerSelection == "paper") {
+    };
+    if (playerSelection == "paper" && computerSelection == "paper") {
         result.innerText = "Draw! You both selected Paper";
         draw += 1;
-    }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
+    };
+    if (playerSelection == "paper" && computerSelection == "rock") {
         result.innerText = "You Win! Paper beats Rock";
         win += 1;
-    }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
+    };
+    if (playerSelection == "paper" && computerSelection == "scissors") {
         result.innerText = "You Lose! Scissors beats Paper";
         lose += 1;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    };
+    if (playerSelection == "scissors" && computerSelection == "scissors") {
         result.innerText = "Draw! You both selected Scissors";
         draw += 1;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
+    };
+    if (playerSelection == "scissors" && computerSelection == "paper") {
         result.innerText = "You Win! Scissors beat Paper";
         win += 1;
-    }
-    else if (playerSelection == 'scissors' && computerSelection == "rock") {
+    };
+    if (playerSelection == 'scissors' && computerSelection == "rock") {
         result.innerText = "You Lose! Rock beats Scissors";
         lose += 1;
-    }
-    else result.innerText = 'There was an error';
+    };
 };
 
-let refreshButton = document.getElementById('again');
-refreshButton.addEventListener('click', () => location.reload());
+// Event listeners for the buttons
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
+// Play again
+document.getElementById('again').addEventListener('click', () => location.reload());
